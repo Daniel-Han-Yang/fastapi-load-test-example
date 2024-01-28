081aa19fc7b64d8ce0b5b8cc38e0e003dafe7eee
Daniel Lim <dalim@seek.com.au>
2024-01-28 12:55:46 +1100

    Larger model for test.

--- readme.md
+++ readme.md
@@ -13,13 +13,13 @@ docker run --gpus=1 -it -p9000:9000 -v $PWD/server:/app/server/ fastapi-transfor
 
 CPU
 ```sh
-docker run -it -p9000:9000 fastapi-transformer bash
+docker run -it -p9000:9000 -v $PWD/server:/app/server/ fastapi-transformer bash
 ```
 
 Run command within container
 ```sh
 cd server
-uvicorn async_fast_1:app --host 0.0.0.0 --port 9000
+uvicorn async_max_1_process:app --host 0.0.0.0 --port 9000
 ```
 
 
---
+++ server/async_max_1_process_bigger.py
@@ -0,0 +1,39 @@
+import asyncio
+import functools
+import time
+from concurrent.futures import ThreadPoolExecutor
+import uvicorn
+
+from fastapi import FastAPI, Request
+from sentence_transformers import SentenceTransformer
+from pydantic import BaseModel
+
+# if you try to run all predicts concurrently, it will result in CPU trashing.
+pool = ThreadPoolExecutor(max_workers=1)
+
+app = FastAPI()
+
+sbertmodel = SentenceTransformer('intfloat/e5-large-v2')
+
+
+class Input(BaseModel):
+    text: str
+
+
+def model_predict(text: str):
+    ts = time.time()
+    vector = sbertmodel.encode(sentences=[text])
+    print(f"Inner model  : {int((time.time() - ts) * 1000)}ms")
+    return vector
+
+
+@app.post("/")
+async def entrypoint(input: Input):
+    loop = asyncio.get_event_loop()
+    ts = time.time()
+    vector = await loop.run_in_executor(pool, functools.partial(model_predict, text=input.text))
+    return {"response": vector.tolist()}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)
